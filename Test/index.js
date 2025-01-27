2568b9b29749a4a64ae8b40d21cbd78452c216a2
Dominic Griesel <dominic.griesel@nabucasa.com>
2025-01-27 09:45:08 +0100

    move business logic into separate function

--- Test/index.js
+++ Test/index.js
@@ -75,103 +75,104 @@ const driver = new Driver(port, {
   allowBootloaderOnly: true,
 })
   .on("error", console.error)
-  .once("driver ready", async () => {
-    const start = new Date();
-    await fs.appendFile(
-      "geoloc.csv",
-      `Time, Latitude, Longitude, Altitude, TxPower, RSSI, ${start}\n`,
-    );
-
-    let txpower = 0;
-    let rssi = 0;
-    const node = driver.controller.nodes.get(DUTNodeID);
-
-    while (true) {
-      // Create a custom command with raw payload
-      const cc = new CommandClass({
-        nodeId: DUTNodeID,
-        ccId: CommandClasses["Geographic Location"],
-        ccCommand: 0x02, // Get
-      });
-
-      // Set up a listener for the response, returning null if it times out
-      const response = driver
-        .waitForCommand(
-          (cc) =>
-            cc.ccId === CommandClasses["Geographic Location"]
-            && cc.ccCommand === 0x03,
-          5000,
-        )
-        .catch(() => null);
-
-      try { // ignore errors on a NACK
-        // Send the command
-        await driver.sendCommand(cc, {
-          // Extract the TX Power and RSSI from the TX Report when it is received
-          onTXReport: (report) => {
-            // The values behind ?? are used in case the info is not present in the report
-            txpower = report.txPower ?? 0;
-            rssi = report.ackRSSI ?? 0;
-          },
-        });
-      } catch (error) {
-        console.log("NACK");
-        await setTimeout(SecondsPerSample * 1000); // wait
-        continue;
-      }
-
-      // Extract the payload of the command if it was received
-      const payload = (await response)?.payload;
-      if (payload) {
-        // convert payload into .csv file coordinates
-        const lon = payload.readInt32BE(0) / (1 << 23); // convert the fixed point value to floating point - the decimal point is bit 23
-        const lat = payload.readInt32BE(4) / (1 << 23);
-        const alt = payload.readIntBE(8, 3) / 100; // reading is in centimeters but most tools want meters
-        const now = new Date();
-        const time = now.toLocaleTimeString("en-US", {
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-        });
-        const stat = payload[11];
-
-        if (0x07 == (stat & 0x07)) { // ignore the reading if the GPS coordinates are not valid
-          await fs.appendFile(
-            "geoloc.csv",
-            `${time}, ${lat}, ${lon}, ${alt}, ${txpower}, ${rssi}\n`,
-          ); // write coords to the csv file
-        }
-
-        console.log("Lat=", lat, "Lon=", lon, "alt=", alt, "Sats=", stat>>4, "TxPower=",txpower);
-      }
-
-      await setTimeout(SecondsPerSample * 1000 / 2); // wait
-/*
-      // send an indicator to blink three times for a visual indicator that the DUT is still in range
-      try {
-        await node.commandClasses.Indicator.set([
-          {
-            indicatorId: 0x50,
-            propertyId: 0x03,
-            value: 2,
-          },
-          {
-            indicatorId: 0x50,
-            propertyId: 0x04,
-            value: 3,
-          },
-        ]);
-      } catch (error) {
-        console.log("NACK");
-      }
-
-      // Wait before sending the next command
-      await setTimeout(SecondsPerSample * 1000 / 2);
-*/
-    }
-  });
-
+  .once("driver ready", main);
 void driver.start();
+
+async function main() {
+  const start = new Date();
+  await fs.appendFile(
+    "geoloc.csv",
+    `Time, Latitude, Longitude, Altitude, TxPower, RSSI, ${start}\n`,
+  );
+
+  let txpower = 0;
+  let rssi = 0;
+  const node = driver.controller.nodes.get(DUTNodeID);
+
+  while (true) {
+    // Create a custom command with raw payload
+    const cc = new CommandClass({
+      nodeId: DUTNodeID,
+      ccId: CommandClasses["Geographic Location"],
+      ccCommand: 0x02, // Get
+    });
+
+    // Set up a listener for the response, returning null if it times out
+    const response = driver
+      .waitForCommand(
+        (cc) =>
+          cc.ccId === CommandClasses["Geographic Location"]
+          && cc.ccCommand === 0x03,
+        5000,
+      )
+      .catch(() => null);
+
+    try { // ignore errors on a NACK
+      // Send the command
+      await driver.sendCommand(cc, {
+        // Extract the TX Power and RSSI from the TX Report when it is received
+        onTXReport: (report) => {
+          // The values behind ?? are used in case the info is not present in the report
+          txpower = report.txPower ?? 0;
+          rssi = report.ackRSSI ?? 0;
+        },
+      });
+    } catch (error) {
+      console.log("NACK");
+      await setTimeout(SecondsPerSample * 1000); // wait
+      continue;
+    }
+
+    // Extract the payload of the command if it was received
+    const payload = (await response)?.payload;
+    if (payload) {
+      // convert payload into .csv file coordinates
+      const lon = payload.readInt32BE(0) / (1 << 23); // convert the fixed point value to floating point - the decimal point is bit 23
+      const lat = payload.readInt32BE(4) / (1 << 23);
+      const alt = payload.readIntBE(8, 3) / 100; // reading is in centimeters but most tools want meters
+      const now = new Date();
+      const time = now.toLocaleTimeString("en-US", {
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+      });
+      const stat = payload[11];
+
+      if (0x07 == (stat & 0x07)) { // ignore the reading if the GPS coordinates are not valid
+        await fs.appendFile(
+          "geoloc.csv",
+          `${time}, ${lat}, ${lon}, ${alt}, ${txpower}, ${rssi}\n`,
+        ); // write coords to the csv file
+      }
+
+      console.log("Lat=", lat, "Lon=", lon, "alt=", alt, "Sats=", stat>>4, "TxPower=",txpower);
+    }
+
+    await setTimeout(SecondsPerSample * 1000 / 2); // wait
+/*
+    // send an indicator to blink three times for a visual indicator that the DUT is still in range
+    try {
+      await node.commandClasses.Indicator.set([
+        {
+          indicatorId: 0x50,
+          propertyId: 0x03,
+          value: 2,
+        },
+        {
+          indicatorId: 0x50,
+          propertyId: 0x04,
+          value: 3,
+        },
+      ]);
+    } catch (error) {
+      console.log("NACK");
+    }
+
+    // Wait before sending the next command
+    await setTimeout(SecondsPerSample * 1000 / 2);
+*/
+  }
+}
 
 // Destroy the driver instance when the application gets a SIGINT or SIGTERM signal
 for (const signal of ["SIGINT", "SIGTERM"]) {
